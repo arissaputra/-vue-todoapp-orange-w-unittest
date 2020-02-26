6ce3098c0fca9bea879d8f3c89239daa37d7ced2
arissaputra <john.arissaputra@gmail.com>
2020-02-26 09:07:34 +0800

    modif server setting

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // server.js
 let express = require('express');
-let path = require('path');
-let serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname + "/dist"));
+let app = express();
+let history = require('connect-history-api-fallback');
+app.use(history({
+    // verbose: true
+}));
+app.use(express.static(__dirname + '/dist'));
 let port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+app.set('port', port);
+app.listen(app.get('port'), () => {
+  console.log(`ENV: ${process.env.NODE_ENV}`);
+  console.log('server started '+ port);
+});
